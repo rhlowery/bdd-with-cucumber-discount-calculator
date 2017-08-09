edf69082b860035333b19505be9358ea9faf1080
James Byars <jamesbyars@g.harvard.edu>
2017-08-09 09:19:23 -0500

    Un-taco-shop the project

--- src/main/java/com/ernesttech/example/bdd/discounts/Policy.java
+++ src/main/java/com/ernesttech/example/bdd/discounts/Policy.java
@@ -5,46 +5,32 @@ import java.math.BigDecimal;
 public class Policy {
 
     private BigDecimal total;
-    private int tacoCount;
-    private boolean drinkIncluded;
     private BigDecimal discount;
 
     public Policy() {
         this.total = BigDecimal.valueOf(0);
-        this.tacoCount = 0;
-        this.drinkIncluded = false;
         this.discount = BigDecimal.valueOf(0);
     }
 
     public Policy(BigDecimal total, int tacoCount, boolean drinkIncluded) {
         this.total = total;
-        this.tacoCount = tacoCount;
-        this.drinkIncluded = drinkIncluded;
         this.discount = BigDecimal.valueOf(0);
     }
 
     public BigDecimal getTotal() {
         return total;
     }
+
     public void setTotal(BigDecimal total) {
         this.total = total;
-    }
-
-    public int getTacoCount() { return tacoCount; }
-    public void setTacoCount(int tacoCount) {
-        this.tacoCount = tacoCount;
-    }
-
-    public boolean isDrinkIncluded() {
-        return drinkIncluded;
-    }
-    public void setDrinkIncluded(boolean drinkIncluded) {
-        this.drinkIncluded = drinkIncluded;
     }
 
     public BigDecimal getDiscount() {
         return discount;
     }
-    public void setDiscount(BigDecimal discount) { this.discount = discount; }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
 }
 
